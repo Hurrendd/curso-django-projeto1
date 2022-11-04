d79687fe999adffb7ee545f987b5fd1c1b78cd9a
Hurrendd de Sousa Ramos <hurrendd.ramos@gmail.com>
2022-11-03 22:53:29 -0300

    Atualização ate a aula 26 _FINAL_ secao 5

--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', context={'name': 'Hurrendd de Sousa Ramos'})
-
-
-def contato(request):
-    return render(request, 'temp.html', context={'msg': 'Esta é apenas uma pagina de teste'})
-
-
-def sobre(request):
-    return render(request, 'home2.html')
