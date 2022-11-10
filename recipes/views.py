0e957849c44881ac87d7341fd3d6824accc88531
Hurrendd de Sousa Ramos <hurrendd.ramos@gmail.com>
2022-11-09 23:24:56 -0300

    Atualização ate a aula 51 _FINAL_ secao 8

--- recipes/views.py
+++ recipes/views.py
@@ -12,4 +12,8 @@ def home(request):
 
 def recipe(request, id: int):
 
-    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe()})
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+
+    })
