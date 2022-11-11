4bc63bd8cfd3bdebd4970ed6f4beaae936b89ac7
Hurrendd de Sousa Ramos <hurrendd.ramos@gmail.com>
2022-11-10 23:20:51 -0300

    Atualização ate a aula 54 _FINAL_ secao 8

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Category, Recipe
+
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Recipe, RecipeAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Recipe(models.Model):
@@ -17,3 +25,7 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
